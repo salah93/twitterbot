5cb409a5b7b57d28d8f669072379ea0e7b3b4d61
salah ahmed <salah93@crosscompute.com>
2019-08-10 09:51:57 -0400

    do not rely on environment variables for tests

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,20 +1,16 @@
 import pytest
 import httpretty
-from os import environ
 
 from twitterbot.TwitterBot import TwitterBot
 
 
 @pytest.fixture
 def bot():
-    consumer_key = environ["TWITTER_CONSUMER_KEY"]
-    consumer_secret = environ["TWITTER_CONSUMER_SECRET"]
-    key = environ["TWITTER_ACCESS_TOKEN"]
-    secret = environ["TWITTER_ACCESS_TOKEN_SECRET"]
-    for k in [consumer_secret, consumer_key, key, secret]:
-        assert k
-    bot = TwitterBot(consumer_key, consumer_secret, key, secret)
-    return bot
+    consumer_key = "consumer_key"
+    consumer_secret = "consumer_secret"
+    key = "key"
+    secret = "secret"
+    return TwitterBot(consumer_key, consumer_secret, key, secret)
 
 
 @pytest.fixture
